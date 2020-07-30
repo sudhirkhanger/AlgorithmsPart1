e3c72848f5c4e9fe817ef0ed24bfde52d364d7e0
Sudhir Khanger <sudhir@sudhirkhanger.com>
2020-07-30 22:43:45 +0530

    project 3 collinear compareTo

---
+++ collinear/BruteCollinearPoints.java
@@ -0,0 +1,18 @@
+public class BruteCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        // todo throw if points null or repeat points.
+        // if (points == null) throw new IllegalArgumentException();
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return -1;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return null;
+    }
+}
