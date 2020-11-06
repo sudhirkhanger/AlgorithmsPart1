a4454a751aade80d4f91cf925cff3d6658fa3a13
Sudhir Khanger <sudhir@sudhirkhanger.com>
2020-11-06 09:32:20 +0530

    equals

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -66,7 +66,16 @@ public class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
-        return false;
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        Board that = (Board) y;
+        if (that.n != n) return false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                if (that.tiles[i][j] != tiles[i][j]) return false;
+        }
+        return true;
     }
 
     // all neighboring boards
@@ -102,6 +111,34 @@ public class Board {
 
         // Test manhattan distance
         StdOut.println("manhattan " + initial.manhattan());
+
+        // Test manhattan distance
+        StdOut.println("isGoal " + initial.isGoal());
+
+        // Test equals
+        int testBoardSize = 4;
+        int[][] testTiles = new int[testBoardSize][testBoardSize];
+        for (int i = 0; i < testBoardSize; i++) {
+            for (int j = 0; j < testBoardSize; j++)
+                testTiles[i][j] = i * testBoardSize + j + 1;
+        }
+        testTiles[testBoardSize - 1][testBoardSize - 1] = 0;
+        Board testBoard = new Board(testTiles);
+
+        StdOut.println("equals " + initial.equals(testBoard));
+
+        for (int i = 0; i < testBoardSize; i++) {
+            for (int j = 0; j < testBoardSize; j++)
+                StdOut.print(initial.tiles[i][j] + " ");
+        }
+
+        StdOut.println();
+
+        for (int i = 0; i < testBoardSize; i++) {
+            for (int j = 0; j < testBoardSize; j++)
+                StdOut.print(testBoard.tiles[i][j] + " ");
+        }
+        StdOut.println();
     }
 
     class Pair {
